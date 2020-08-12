8f1ba116a487f8f1b22a71cabd97f4e0cdc11f59
slacey1974 <69576217+slacey1974@users.noreply.github.com>
2020-08-12 10:48:49 +0100

    Create VixexerciseApplication.java

---
+++ src/main/java/com/vix/exercise/VixexerciseApplication.java
@@ -0,0 +1,13 @@
+package com.vix.exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VixexerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VixexerciseApplication.class, args);
+	}
+
+}
