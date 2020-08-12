95405641b3c298bb5508e25ddbb9b45b35a93ada
slacey1974 <69576217+slacey1974@users.noreply.github.com>
2020-08-12 10:59:37 +0100

    Create VixexerciseApplicationTests.java

---
+++ src/tests/java/com/vix/exercise/VixexerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vix.exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VixexerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
